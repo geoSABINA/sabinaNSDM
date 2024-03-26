9a492cb095565e117a5816e9714ce18231206254
TGoicolea <163032086+TGoicolea@users.noreply.github.com>
2024-03-26 11:13:23 +0100

    teresa changes

--- R/MultiplyModel.R
+++ R/MultiplyModel.R
@@ -1,30 +1,59 @@
 #' @export
-NSH.SDM.Multiply.Models <- function(SpeciesName,
-				    method="Arithmetic",
-				    rescale=FALSE) {
+NSH.SDM.Multiply.Models <- function(nshsdm_global,
+                                    nshsdm_regional,
+                                    #SpeciesName, #@@@#TG charge from objects
+                                    method="Arithmetic",
+                                    rescale=FALSE,
+                                    save.output=TRUE) {
+
+  SpeciesName <- nshsdm_regional$Species.Name
+  if(SpeciesName != nshsdm_regional$Species.Name){
+    stop("Different species in global and regional levels")
+  }
+
+  if(!inherits(nshsdm_regional, "nshsdm.predict")){
+    stop("nshsdm_regional must be an object of nshsdm.predict class.")
+  }
+
+  if(!inherits(nshsdm_global, "nshsdm.predict")){
+    stop("nshsdm_global must be an object of nshsdm.predict class.")
+  }
+
 
   nshsdm_data<-list()
+  nshsdm_data$args <- list()
+  nshsdm_data$args$method <- method
+  nshsdm_data$args$rescale <- rescale
 
-  tryCatch({ 
-  	Scenarios <- dir_ls(paste0(VariablesPath,"/Regional"))
-  	Scenarios <- path_file(Scenarios) |> path_ext_remove()
-	Scenarios <- Scenarios
+  nshsdm_data$link <- list()
+  nshsdm_data$link$multiply.models <- "/Results/Multiply/Projections/"
 
-  	walk(Scenarios, function(projmodel) {
-	  #projmodel <- Scenarios[1]
-	  # Load raster dataat global scale
-	  Pred.global <- terra::rast(paste0("Results/Global/Projections/",SpeciesName,".",projmodel,".tif"))
-    
-	  # Load raster at regional scale
-	  Pred.regional <- terra::rast(paste0("Results/Regional/Projections/",SpeciesName,".",projmodel,".tif"))
-    
+  tryCatch({
+  	Scenarios <- sapply(nshsdm_regional$new.projections$Pred.Scenario, names) #dir_ls(paste0(VariablesPath,"/Regional"))
+  	Scenarios <- gsub(paste0(SpeciesName,"."),"",Scenarios) #path_file(Scenarios) |> path_ext_remove()
+	  Scenarios <- c("Current",Scenarios)
+
+	  for (i in 1:length(Scenarios)) {
+
+  	#walk(Scenarios, function(projmodel) {
+	  projmodel <- Scenarios[i]
+	  # Load raster data at global and regional scales
+	  if (projmodel =="Current") {
+	    Pred.global <- nshsdm_global$current.projections$Pred #terra::rast(paste0("Results/Global/Projections/",SpeciesName,".",projmodel,".tif"))
+	    Pred.regional <- nshsdm_regional$current.projections$Pred #terra::rast(paste0("Results/Regional/Projections/",SpeciesName,".",projmodel,".tif"))
+
+	    } else {
+	    Pred.global <- nshsdm_global$new.projections$Pred.Scenario[[i-1]]
+	    Pred.regional <- nshsdm_regional$new.projections$Pred.Scenario[[i-1]]
+	  }
+
 	  # Rescale global prediction to a common range
 	  if(rescale==FALSE) {
 	    Pred.global <- Pred.global
 	    Pred.regional <- Pred.regional
 	  } else if(rescale==TRUE) {
 	    # Calculate minimum and maximum values of global prediction
-	    min_val <- min(values(Pred.global), na.rm = TRUE)
+	    min_val <- min(terra::values(Pred.global), na.rm = TRUE)
 	    max_val <- max(values(Pred.global), na.rm = TRUE)
 
 	    Pred.global <- terra::app(Pred.global, fun = function(x) {
@@ -35,41 +64,70 @@ NSH.SDM.Multiply.Models <- function(SpeciesName,
 
 	    Pred.regional <- terra::app(Pred.regional, fun = function(x) {
 	    ((x - min_val) / (max_val - min_val) * 999) + 1})
-      	  } 
-    
-	  #Create outoput folder
-    	  dir_create(c("Results/Multiply/Projections/"))
-    
+      	  }
+
     	  # Average global and regional
-    	  if(method=="Geometric") { 
-    	    Stack.rasters <- c(Pred.global, Pred.regional) 
+    	  if(method=="Geometric") {
+    	    Stack.rasters <- c(Pred.global, Pred.regional)
     	    res.average <-  terra::app(Stack.rasters, function(...) exp(mean(log(c(...)))))
     	  } else if(method=="Arithmetic")  {
     	    Stack.rasters <- c(Pred.global, Pred.regional)
     	    res.average <-  terra::mean(Stack.rasters)
+    	  }
+
+	  res.average<-terra::rast(wrap(res.average))
+	  if (projmodel =="Current") {
+	    nshsdm_data$current.projections$Pred <- setNames(res.average, paste0(SpeciesName, ".Current"))
+	  } else {
+	    nshsdm_data$new.projections$Pred.Scenario[[i]]<- setNames(res.average, paste0(SpeciesName,".", projmodel))
 	  }
-          terra::writeRaster(res.average, paste0("Results/Multiply/Projections/",SpeciesName,".",projmodel,".tif"), overwrite = TRUE)
-  	}) 
 
-  nshsdm_data$args <- list()
-  nshsdm_data$args$method <- method
-  nshsdm_data$args$rescale <- rescale
+	  if(save.output){
+	    #Create outoput folder
+	    dir_create(c("Results/Multiply/Projections/"),recurse=TRUE)
+	    file_path<-paste0("Results/Multiply/Projections/",SpeciesName,".",projmodel,".tif")
+	    terra::writeRaster(res.average, file_path, overwrite = TRUE)
+	    message(paste("Multiply projections under",Scenarios[i] ,"conditions saved in:",file_path,cat("\033[1;34m")))
+	    cat("\033[0m")
+	  }
+  #	}) # walk
+} #for
 
-  nshsdm_data$link <- list()
-  nshsdm_data$link$multiply.models <- "/Results/Multiply/Projections/"
+  # Logs success or error messages
+  message("\nNSH.SDM.Multiply.Models executed successfully!\n",cat("\033[32m"))
+  cat("\033[0m")
 
-  # Logs success or error messages 
-  message("\nNSH.SDM.Multiply.Models executed successfully!\n")
-  message("Results saved in the following locations:")
+  if(save.output){
+  message("Results saved in the following locations:",cat("\033[1;34m"))
   message(paste(
     " - Hierarchical Multiply Models: /Results/Multiply/Projections/\n"
-  )) 
+  ),cat("\033[1;34m"))
+  cat("\033[0m")
+  }
 
   }, error = function(err) {
   message("\nError in NSH.SDM.Multiply.Models:", conditionMessage(err))
   return(list(result = NULL, error = err))
   })
 
+  results<-nshsdm_global$Summary #@@@# careful! This is not charging the summaries of global!
+  results<-rbind(results,
+                 c("Statistical algorithms at regional level",paste(nshsdm_regional$args$models,collapse = ", ")),
+                 c("Number of replicates wit AUC > 0.8 at regional level",nrow(nshsdm_regional$myEMeval.replicates)),
+                 c("AUC of ensemble modle at regional level",nshsdm_regional$myEMeval.Ensemble$calibration[which(nshsdm_regional$myEMeval.Ensemble$metric.eval=="ROC")]),
+                 c("TSS of ensemble modle at regional level",nshsdm_regional$myEMeval.Ensemble$calibration[which(nshsdm_regional$myEMeval.Ensemble$metric.eval=="TSS")]),
+                 c("KAPPA of ensemble modle at regional level",nshsdm_regional$myEMeval.Ensemble$calibration[which(nshsdm_regional$myEMeval.Ensemble$metric.eval=="KAPPA")]),
+                 c("Multiply method",method), #@@@# we need to calculate the evaluation metrics for multiply
+                 c("AUC of hierarchical multiply ensemble modle","@@@@"), #@@@# we need to calculate the evaluation metrics for multiply
+                 c("TSS of hierarchical multiply ensemble modle","@@@@"),
+                 c("KAPPA of hierarchical multiply ensemble modle","@@@"))
+
+  if(save.output){
+    write.table(results, paste0("Results/",SpeciesName,"_summary.csv"), sep=",",  row.names=F, col.names=T)
+  }
+  nshsdm_data$Summary<-results
+  nshsdm_selvars$Summary<-results
+
   return(nshsdm_data)
 
-} 
+}
--- R/PrepareData.R
+++ R/PrepareData.R
@@ -3,22 +3,21 @@ NSH.SDM.PrepareData <- function(VariablesPath,
 				SpeciesFilePath,
 				SpeciesName,
 				nPoints=10000,
-				Min.Dist.Global=NULL,
-				Min.Dist.Regional=NULL,
+				Min.Dist.Global="Resolution",
+				Min.Dist.Regional="Resolution",
 				Background.Global=NULL,
 				Background.Regional=NULL,
 				save.output=TRUE) {
 
   if (!is.null(Background.Global) && !(is.data.frame(Background.Global) && ncol(Background.Global) == 2 && all(c("x", "y") %in% names(Background.Global)))) {
-    stop("background must be a data.frame with two columns 'x' and 'y'.")
+    stop("Background.Global must be a data.frame with two columns 'x' and 'y' or NULL.")
   }
 
   if (!is.null(Background.Regional) && !(is.data.frame(Background.Regional) && ncol(Background.Regional) == 2 && all(c("x", "y") %in% names(Background.Regional)))) {
-    stop("background must be a data.frame with two columns 'x' and 'y'.")
+    stop("Background.Regional must be a data.frame with two columns 'x' and 'y' or NULL.")
   }
-
+  results<-data.frame(Name="Species name",Value=SpeciesName)
   sabina_data<-list()
-
   if(save.output){
   dir_create(c("Results/", #@@@RGM no quitar estas carpetas
                "Results/Global/", #@@@RGM no quitar estas carpetas
@@ -42,22 +41,41 @@ NSH.SDM.PrepareData <- function(VariablesPath,
   IndVar.Global <- IndVar.Global[[names(IndVar.Global)]]
   Mask.Global <- prod(IndVar.Global, 1)
   IndVar.Global <- terra::mask(IndVar.Global, Mask.Global) #@RGM creo que esto es necesario para evitar NODATA
+
   # Generate random background points for model calibration
   if(is.null(Background.Global)) {
     Valid.Cells.Global <- which(!is.na(values(Mask.Global)))
     if(length(Valid.Cells.Global) < nPoints) {
-      stop("The requested number of bockground points exceeds the number of valid cells.")
+      stop(paste("The requested number of bockground points exceeds the number of valid cells. Maximum number of background points:",length(Valid.Cells.Global)))
     }
     Sampled.indices.Global <- sample(Valid.Cells.Global, nPoints)
     Coords.Global <- terra::xyFromCell(Mask.Global, Sampled.indices.Global)
     Background.XY.Global <- as.data.frame(Coords.Global)
   } else {
-    Background.XY.Global <- Background.Global
+    #remova NAs
+    XY.Global <- terra::extract(Mask.Global, Background.Global, xy=TRUE, na.rm=TRUE)
+    XY.Global <- na.omit(XY.Global)[, -c(1:2)]
+    XY.Global <- unique(XY.Global)
+    # Spatial thinning of background data to remove duplicates and apply minimum distance criteria
+    if(Min.Dist.Global == "Resolution" ) {
+      Min.Dist.Global<-res(Mask.Global)[1]}
+    invisible(capture.output({
+      tryCatch({
+        XY.final.Global <- ecospat::ecospat.occ.desaggregation(XY.Global, min.dist = Min.Dist.Global, by = NULL)
+      }, error = function(e) {
+        # If an error occurs, run the alternative block
+        XY.Global <- round(XY.Global, digits = 4)
+        XY.final.Global <- ecospat::ecospat.occ.desaggregation(XY.Global, min.dist = Min.Dist.Global, by = NULL)
+      })
+    }))
+    Background.XY.Global<-XY.final.Global
+    message(paste("Global background data thinning: from", nrow(Background.Global), "to", nrow(Background.XY.Global)))
   }
 
   if(save.output){
   write.csv(Background.XY.Global,  paste0("Results/Global/Background/Background.csv"))
   }
+
 
 # Load species data at global scale
   SpeciesData.XY.Global <- read.csv(paste0(SpeciesFilePath,"/Global/", SpeciesName, ".csv"))
@@ -69,7 +87,7 @@ NSH.SDM.PrepareData <- function(VariablesPath,
   XY.Global <- unique(XY.Global)
 
   # Spatial thinning of presence data to remove duplicates and apply minimum distance criteria
-  if(is.null(Min.Dist.Global)) {
+  if(Min.Dist.Global == "Resolution" ) {
     Min.Dist.Global<-res(Mask.Global)[1]
   }
 
@@ -83,7 +101,11 @@ NSH.SDM.PrepareData <- function(VariablesPath,
     })
   }))
   message(paste("Global data thinning: from", nrow(XY.Global), "to", nrow(XY.final.Global), "species presences"))
-
+results<-rbind(results,
+               c("Original number of species presences at global level",nrow(XY.Global)),
+               c("Final number of species presences at global level",nrow(XY.final.Global)),
+               c("Original number of background points at global level",nrow(Background.Global)),
+               c("Final number of background points global level",nrow(Background.XY.Global)))
   # Save thinning presence data for each species
   if(save.output){
   write.csv(XY.final.Global, paste0("Results/Global/SpeciesXY/",SpeciesName,".csv"))
@@ -100,20 +122,37 @@ NSH.SDM.PrepareData <- function(VariablesPath,
   # Regional independent variables (environmental layers)
   IndVar.Regional <- terra::rast(paste0(VariablesPath,"/Regional/Current.tif"))
   IndVar.Regional <- IndVar.Regional[[names(IndVar.Regional)]]
-  Mask.regional <- prod(IndVar.Regional)
-  IndVar.Regional <- terra::mask(IndVar.Regional, Mask.regional) #@RGM creo que esto es necesario para evitar NODATA
+  Mask.Regional <- prod(IndVar.Regional)
+  IndVar.Regional <- terra::mask(IndVar.Regional, Mask.Regional) #@RGM creo que esto es necesario para evitar NODATA
 
   # Generate random background points for model calibration
   if(is.null(Background.Regional)) {
-    Valid.Cells.Regional <- which(!is.na(values(Mask.regional)))
+    Valid.Cells.Regional <- which(!is.na(values(Mask.Regional)))
     if(length(Valid.Cells.Regional) < nPoints) {
       stop("The requested number of bockground points exceeds the number of valid cells.")
     }
     Sampled.indices.Regional <- sample(Valid.Cells.Regional, nPoints)
-    Coords.Regional <- terra::xyFromCell(Mask.regional, Sampled.indices.Regional)
+    Coords.Regional <- terra::xyFromCell(Mask.Regional, Sampled.indices.Regional)
     Background.XY.Regional <- as.data.frame(Coords.Regional)
   } else {
-    Background.XY.Regional <- Background.Regional
+    #remova NAs
+    XY.Regional <- terra::extract(Mask.Regional, Background.Regional, xy=TRUE, na.rm=TRUE)
+    XY.Regional <- na.omit(XY.Regional)[, -c(1:2)]
+    XY.Regional <- unique(XY.Regional)
+    # Spatial thinning of background data to remove duplicates and apply minimum distance criteria
+    if(Min.Dist.Regional == "Resolution" ) {
+      Min.Dist.Regional<-res(Mask.Regional)[1]}
+    invisible(capture.output({
+      tryCatch({
+        XY.final.Regional <- ecospat::ecospat.occ.desaggregation(XY.Regional, min.dist = Min.Dist.Regional, by = NULL)
+      }, error = function(e) {
+        # If an error occurs, run the alternative block
+        XY.Regional <- round(XY.Regional, digits = 4)
+        XY.final.Regional <- ecospat::ecospat.occ.desaggregation(XY.Regional, min.dist = Min.Dist.Regional, by = NULL)
+      })
+    }))
+    Background.XY.Regional<-XY.final.Regional
+    message(paste("Regional background data thinning: from", nrow(Background.Regional), "to", nrow(Background.XY.Regional)))
   }
 
   if(save.output){
@@ -125,14 +164,14 @@ NSH.SDM.PrepareData <- function(VariablesPath,
   names(SpeciesData.XY.Regional) <- c("x","y")
 
   # Occurrences from sites with no NA
-  XY.Regional <- terra::extract(Mask.regional, SpeciesData.XY.Regional, xy=TRUE, na.rm=TRUE)
+  XY.Regional <- terra::extract(Mask.Regional, SpeciesData.XY.Regional, xy=TRUE, na.rm=TRUE)
   XY.Regional <- na.omit(XY.Regional)[, -c(1:2)]
   XY.Regional <- unique(XY.Regional)
 
   # Spatial thinning of presence data to remove duplicates and apply minimum distance criteria
 
-  if(is.null(Min.Dist.Regional)) {
-  Min.Dist.Regional<-res(Mask.regional)[1]
+  if(Min.Dist.Regional=="Resolution") {
+  Min.Dist.Regional<-res(Mask.Regional)[1]
   }
     invisible(capture.output({
       tryCatch({
@@ -146,6 +185,11 @@ NSH.SDM.PrepareData <- function(VariablesPath,
     }))
     message(paste("Regional data thinning: from", nrow(XY.Regional), "to", nrow(XY.final.Regional), "species presences"))
 
+     results<-rbind(results,
+                   c("Original number of species presences at regional level",nrow(XY.Regional)),
+                   c("Final number of species presences at regional level",nrow(XY.final.Regional)),
+                   c("Original number of background points at regional level",nrow(Background.Regional)),
+                   c("Final number of background points regional level",nrow(Background.XY.Regional)))
   # Save filtered presence data for each species
 
   if(save.output){
@@ -158,6 +202,22 @@ NSH.SDM.PrepareData <- function(VariablesPath,
   write.table(Sample.size.Regional, paste0("Results/Regional/Values/",SpeciesName,"_samplesize.csv"), sep=",",  row.names=F, col.names=T)
   }
 
+  Scenarios <- dir_ls(paste0(VariablesPath,"/Regional"), pattern="tif") #@@@# change this so it comes from an object
+  Scenarios <- Scenarios[!grepl("Current.tif", Scenarios)]
+  if(length(Scenarios) == 0) {
+    message("There are no future scenarios",	cat("\033[0m"))
+  } else  {
+    message("Future scenarios: ",cat("\033[1;34m"))
+    print(path_ext_remove(path_file(Scenarios)),cat("\033[1;34m"))
+    cat("\033[0m")
+  }
+  results<-rbind(results,
+                 c("Number of new scenarios",length(Scenarios)),
+                 c("Variables path",VariablesPath))
+  if(save.output){
+    write.table(results, paste0("Results/",SpeciesName,"_summary.csv"), sep=",",  row.names=F, col.names=T)
+  }
+  sabina_data$Summary<-results
   sabina_data$Species.Name <- SpeciesName
   sabina_data$VariablesPath <- VariablesPath
   sabina_data$SpeciesData.XY.Global <- XY.final.Global
@@ -166,9 +226,27 @@ NSH.SDM.PrepareData <- function(VariablesPath,
   sabina_data$Background.XY.Regional <- Background.XY.Regional
   sabina_data$IndVar.Global <- IndVar.Global
   sabina_data$IndVar.Regional <- IndVar.Regional
+  sabina_data$Scenarios<-	Scenarios
 
   attr(sabina_data, "class") <- "nshsdm.input"
 
+   # Logs success or error messages
+  # message("\nPrepareData executed successfully!\n",cat("\033[32m"))
+  # cat("\033[0m")
+  # if(save.output){
+  #   message("Results saved in the following locations:",cat("\033[1;34m"))
+  #   message(paste0(
+  #     " - Global sample size: Results/Global/Values/\n",
+  #     " - Global background points: Results/Global/Background/Background.csv\n",
+  #     " - Global species occurrences: Results/Global/SpeciesXY/\n",
+  #     " - Regional sample size: Results/Regional/Values/\n",
+  #     " - Regional background points: Results/Regional/Background/Background.csv\n",
+  #     " - Regional species occurrences: Results/Regional/SpeciesXY/\n"
+  #   ),cat("\033[1;34m"))
+  #   cat("\033[0m")
+  #}
+
   return(sabina_data)
 
-}
+  }
+
