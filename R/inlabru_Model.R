e8168de8b56f70924924209d4b36104270c3a951
Virgilio Gomez Rubio <virgil@iMac-de-Virgilio.local>
2024-05-28 12:19:45 +0200

    Initial commit.

---
+++ R/inlabru_Model.R
@@ -0,0 +1,38 @@
+#' @name inlabru.Regional
+#'
+#' @title
+#'
+#' @description 
+#'
+#'
+#' @param
+#'
+#' @param
+#'
+#' @param
+#'
+#' @param
+#'
+#' @param
+#'
+#' @param
+#'
+#' @param
+#'
+#' @param
+#'
+#'
+#' @return
+#'
+#'
+#' @details
+#'
+#'
+#' @seealso \code{\link{NSDM.InputData}}, \code{\link{NSDM.FormattingData}}, \code{\link{NSDM.SelectCovariates}}
+
+#'
+#'
+#' @examples
+#'
+#'
+#' 
