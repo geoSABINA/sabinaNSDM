1200c4ca0c028d13c8c2f26457ccee2616d5b24b
Jennifer Morales Barbero <118901667+JMoralBa@users.noreply.github.com>
2024-06-19 11:50:21 +0200

    complete and improve documentation data

---
+++ R/Quercus.petraea.xy.regional.R
@@ -0,0 +1,13 @@
+#' Data Quercus.petraea.xy.regional
+#'
+#' A dataset containing the coordinates (x, y) of the occurrence points of the species (\emph{Quercus petraea} at regional level.
+#'
+#' @format ## `Quercus.petraea.xy.regional`
+#' A \code{data.frame} with 775 rows and 2 columns:
+#' \describe{
+#'   \item{x}{Longitude of the occurrence point}
+#'   \item{y}{Latitude of the occurrence point}
+#' }
+#'
+#' @source <https://nsojournals.onlinelibrary.wiley.com/doi/full/10.1111/ecog.07328>
+"Quercus.petraea.xy.regional"
---
+++ R/expl.var.regional.R
@@ -0,0 +1,20 @@
+#' Data expl.var.regional
+#'
+#' A dataset containing multiple environmental covariates at a regional level.
+#'
+#' @format ## `expl.var.regional`
+#' A \code{SpatRaster} object with the following layers:
+#' \describe{
+#'   \item{bio1}{Annual Mean Temperature}
+#'   \item{bio2}{Mean Diurnal Range (Mean of monthly (max temp - min temp))}
+#'   \item{bio3}{Isothermality (BIO2/BIO7) (×100)}
+#'   \item{bio4}{Temperature Seasonality (standard deviation ×100)}
+#'   \item{bio12}{Annual Precipitation}
+#'   \item{sand}{Sand content}
+#'   \item{radiation}{Annual solar radiation}
+#' }
+#'
+#'
+#' @source <https://nsojournals.onlinelibrary.wiley.com/doi/full/10.1111/ecog.07328>
+"expl.var.regional"
+
